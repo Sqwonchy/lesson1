66230fbecf1c27d7bcf7c3c19bb516f2f1e1d2ca
Sqwonchy <ilianos321@gmail.com>
2024-08-19 18:30:12 +0300

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,5 @@
+quantity_completed = int(12)
+time_spent = float(1.5)
+course_name = "Python"
+timing = float(time_spent/quantity_completed)
+print("Курс:",course_name,", всего задач: ",quantity_completed,", затрачено часов ",time_spent,", среднее время выполнения ",timing)
