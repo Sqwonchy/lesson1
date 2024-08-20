6c03d29703ab2323386ef2c47bdead843dbcd6c1
Sqwonchy <ilianos321@gmail.com>
2024-08-20 20:55:27 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = "Илья"
 print("Имя:",name)
 age = 25
 print("Возраст:",age)
-age = 26
+age = (age + 1)
 print("Новый возраст:",age)
 is_student = True
 print("Является студентом:", is_student)
